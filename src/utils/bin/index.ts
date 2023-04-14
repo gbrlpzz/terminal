77eab7b1add1d5b2ef8f4e9b63cc62c9d7b7ce6d
Gabri <gabrielepizzi@protonmail.com>
2023-04-14 13:59:38 +0200

    customization

--- src/utils/bin/index.ts
+++ src/utils/bin/index.ts
@@ -3,6 +3,4 @@ export * from './projects';
 export * from './social';
 export * from './theme';
 export * from './utils';
-export * from './weather';
-export * from './neofetch';
 export * from './cowsay';
